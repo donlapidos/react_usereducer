54b5477a94156b56f203389d404d28eafdcbdf23
donlapidos <donlapidos@gmail.com>
2022-06-20 14:28:36 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  NEW_USER_INPUT: "newUserInput",
+  TG_COLOR: "tgColor"
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { ...state, count: state.count + 1 }
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 }
+    case ACTION.TG_COLOR:
+      return { ...state, color: !state.color }
+    case ACTION.NEW_USER_INPUT:
+      return { ...state, userInput: action.payload }
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, color: false, userInput: "" });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ color: state.color ? "#d1001f" : "#000137" }}>
+      <input
+        type="text"
+        placeholder="type something..."
+        value={state.userInput}
+        onChange={(e) => dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })}
+      />
+
+      <p>{state.count}</p>
+
+      <section>
+        <button onClick={(() => dispatch({ type: ACTION.INCREMENT }))}>+</button>
+        <button onClick={(() => dispatch({ type: ACTION.DECREMENT }))}>-</button>
+        <button onClick={(() => dispatch({ type: ACTION.TG_COLOR }))}>Color</button>
+      </section>
+
+      <p>{state.userInput}</p>
+
     </div>
   );
 }
